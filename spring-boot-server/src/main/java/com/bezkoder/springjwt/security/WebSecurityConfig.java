da4c4671d94502b2bada425f662ebf0d62d92577
Sirish Rajuri <apple@Apples-MacBook-Air.local>
2023-10-29 10:11:17 -0400

    Changed DB from MySQL to H2

--- spring-boot-server/src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
+++ spring-boot-server/src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
@@ -54,13 +54,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable()
-			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
-			.antMatchers("/api/test/**").permitAll()
-			.anyRequest().authenticated();
+//		http.cors().and().csrf().disable()
+//			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+//			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
+//			.antMatchers("/api/test/**").permitAll()
+//			.anyRequest().authenticated();
+//
+//		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
-		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.cors().and().csrf().disable()
+				.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authorizeRequests()
+				.antMatchers("/api/auth/**").permitAll()
+				.antMatchers("/api/test/**").permitAll()
+				.antMatchers("/h2-console/**").permitAll()  // Allow H2 console access
+				.anyRequest().authenticated().and()
+				.headers().frameOptions().disable();
+
+
 	}
 }
